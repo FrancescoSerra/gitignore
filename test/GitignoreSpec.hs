cc17b4f25075361915d737f9e794e9704c18edfa
Francesco Serra <fn.serra@gmail.com>
2025-03-13 14:35:43 +0000

    use custom Computation and FailedComputation for testing

--- test/GitignoreSpec.hs
+++ test/GitignoreSpec.hs
@@ -1,21 +1,55 @@
 module GitignoreSpec where
 
 import CliApp
-import Data.Functor.Identity
 import Gitignore
 import Test.Hspec
 
-testReadIntoNumber :: Identity Int
+testReadIntoNumber :: Computation Int
 testReadIntoNumber = readIntoNumber
 
-testReadIntoNumberFail :: Computation Int
+testReadIntoNumberFail :: FailedComputation Int
 testReadIntoNumberFail = readIntoNumber
 
+-- addPatternFailed :: PlainPattern -> FailedComputation ()
+-- addPatternFailed = addPattern
+
+
 spec :: Spec
-spec = do
+spec = parallel $ do
+  helpers
+  app
+
+helpers :: Spec
+helpers = do
   describe "readIntoNumber" $ do
     it "should read a number from stdin" $
-      testReadIntoNumber `shouldBe` 1
+      testReadIntoNumber `shouldBe` Result 1
 
     it "should handle a non-number input" $
-      testReadIntoNumberFail `shouldBe` Error "Invalid input. Please enter a number."
+      testReadIntoNumberFail `shouldBe` 
+        FailedComputation (Error "Invalid input. Please enter a number.")
+
+app :: Spec
+app = describe "App" $ do
+  context "addPattern" $ do
+    it "should add a pattern to .gitignore" $ do
+      let pattern = PlainPattern "pattern"
+      addPattern pattern `shouldBe` Result ()
+
+    it "should handle an existing pattern" $ do
+      let pattern = PlainPattern "pattern"
+      addPattern pattern `shouldBe` FailedComputation (Error ".gitignore")
+  
+  context "printOut" $ do
+    it "should print the content of .gitignore" $
+      printOut `shouldBe` Result ()
+    
+    it "should handle an error" $
+      printOut `shouldBe` FailedComputation (Error ".gitignore")
+
+  context "removePattern" $ do
+    it "should remove a pattern from .gitignore" $
+      removePattern `shouldBe` Result ()
+
+    it "should handle an error" $
+      removePattern `shouldBe` FailedComputation (Error ".gitignore")
